5a311480960b77edea3a583033556fd5dc142304
Bishesh Khanal <bishesh.khanal25@gmail.com>
2025-02-01 14:12:16 +0545

    Add project files.

---
+++ Game4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello World" << std::endl;
+	//std::shared_ptr<GameEngine> game = std::make_shared<GameEngine>();
+	//game->run(game);
+
+	return 0;
+}
